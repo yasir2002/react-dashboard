6f0d5abcaf137d9d5121455b660809fd046a8284
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-11 23:43:56 +0500

    :lipstick: Added SCSS variables

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>This is a React App.</h1>
+      <h1>This is primary text.</h1>
+      <p>This is secondary text.</p>
     </div>
   );
 }
